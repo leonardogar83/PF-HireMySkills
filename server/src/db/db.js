7eef58cdf6a5529b5145e8e5c778a61be378c4a3
leonardogar83 <leonardogar.ec@gmail.com>
2023-02-08 18:50:52 -0500

    DB_URL postgres connection

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -2,18 +2,13 @@ const { Sequelize } = require("sequelize");
 const { config } = require("dotenv");
 
 config();
-const {
-  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME
-} = process.env
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 const { DB_URL } = process.env;
-const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
-  {
-    dialect: "postgres",
-    native: false,
-    logging: false,
-  }
-);
+const sequelize = new Sequelize(DB_URL, {
+  dialect: "postgres",
+  native: false,
+  logging: false,
+});
 
 // Professional.belongsToMany(Profession, { through: "professional_profession" });
 // Profession.belongsToMany(Professional, { through: "professional_profession" });
