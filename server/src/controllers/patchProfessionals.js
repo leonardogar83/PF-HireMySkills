6050b938321ec124a7630517727315b83538c5cf
AndresAndrada <andresandrada1994@gmail.com>
2023-02-08 09:30:07 -0300

    Prqueñas modificaciones de la ruta patch profesional

--- server/src/controllers/patchProfessionals.js
+++ server/src/controllers/patchProfessionals.js
@@ -21,48 +21,40 @@ const patchProfessionals = async (req, res) => {
             return profesional;
         }
         if(name) {
-            const profesional = await professionals.update({ name: name }, { where: { id: id }});
-            // console.log(profesional, 'profesional name');
+            await professionals.update({ name: name }, { where: { id: id }});
+            
         }
         if(description) {
-            const profesional = await professionals.update({ description: description }, { where: { id: id }});
-            // console.log(profesional, 'profesional name');
+            await professionals.update({ description: description }, { where: { id: id }});
         }
         
         if(photo) {
-            const profesional = await professionals.update({ photo: photo }, { where: { id: id }});
-            // console.log(profesional, 'profesional photo');
+            await professionals.update({ photo: photo }, { where: { id: id }});
         }
         if(email) {
             const profesionalRep = await professionals.findAll({ where: { email: email }})
             if(profesionalRep.length > 0) return res.send({ message: "profesional already exists" });
             // console.log(profesionalRep, 'profesional email');
-            const profesional = await professionals.update({ email: email }, { where: { id: id }});
+            await professionals.update({ email: email }, { where: { id: id }});
         }
         if(town) {
-            const profesional = await professionals.update({ town: town }, { where: { id: id }});
-            // console.log(profesional, 'profesional town');
+            await professionals.update({ town: town }, { where: { id: id }});
         }
         if(state) {
-            const profesional = await professionals.update({ state: state }, { where: { id: id }});
-            // console.log(profesional, 'profesional town');
+            await professionals.update({ state: state }, { where: { id: id }});
         }
         if(country) {
-            const profesional = await professionals.update({ country: country }, { where: { id: id }});
-            // console.log(profesional, 'profesional town');
+            await professionals.update({ country: country }, { where: { id: id }});
         }
         if(contact) {
-            const profesional = await professionals.update({ contact: contact }, { where: { id: id }});
-            // console.log(profesional, 'USER contact');
+            await professionals.update({ contact: contact }, { where: { id: id }});
         }
         if(portfolio) {
-            const profesional = await professionals.update({ portfolio: portfolio }, { where: { id: id }});
-            // console.log(profesional, 'profesional town');
+            await professionals.update({ portfolio: portfolio }, { where: { id: id }});
         }
         if(skills) {
-            console.log(skills, 'SKILLS');
-            const profesional = await professionals.update({ skills: skills }, { where: { id: id }});
-            // console.log(profesional, 'profesional town');
+            
+            await professionals.update({ skills: skills }, { where: { id: id }});
         }
         res.send({ message: 'Modified user' });
     } catch (error) {
