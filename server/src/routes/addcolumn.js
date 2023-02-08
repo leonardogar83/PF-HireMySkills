2f66b3cead52f0dd8f217e22aec063bf53856de5
leonardogar83 <leonardogar.ec@gmail.com>
2023-02-08 12:01:12 -0500

    add column role to database

--- server/src/routes/addcolumn.js
+++ server/src/routes/addcolumn.js
@@ -3,10 +3,14 @@ const sequelize = require("../db/db.js");
 const queryInterface = sequelize.getQueryInterface();
 
 const addcolumn = async (req, res) => {
-  await queryInterface.addColumn("users", "role", {
-    type: DataTypes.ENUM("admin", "user", "professional"),
-  });
-  res.send("created");
+  try {
+    await queryInterface.addColumn("users", "role", {
+      type: DataTypes.ENUM("admin", "user", "professional"),
+    });
+    res.send("created");
+  } catch (error) {
+    res.send(error.message);
+  }
 };
 
 module.exports = addcolumn;
